4fab88c7dfd6125a5fe7d96dc5b73566a9dae89d
Spencer Caretta <spencer.caretta@gmail.com>
2018-11-03 11:26:22 -0500

    minor changes to html and app.js to append to new div

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,15 @@
-var url = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_orbper,pl_bmassj,st_dist,st_teff&order=dec&format=ascii";
+var queryURL = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=exoplanets&select=pl_name,pl_orbper,pl_bmassj,st_dist,st_teff&order=dec&format=ascii";
 var config = {
     apiKey: "EBb20IKo5DUliI4QmZnwQQ4sfvUKIcqfgHdePwb3",
 }
 
 $.ajax({
-    url: url,
+    url: queryURL,
     method: "GET"
 
 })
     .then(function(response){
-        var results = response.data;
-        $("#results").append(url);
+        //var results = response.data;
+        $("#results").append(response);
+        console.log(response);
     })
