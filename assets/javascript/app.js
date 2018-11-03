ff935cc1a160702d4d6c3ada1c1d898f24e4bd5f
Eric King <kingeric027@gmail.com>
2018-11-03 13:04:37 -0500

    Added click listener. Returns variable max and mins

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,3 +13,27 @@ $.ajax({
         $("#results").append(response);
         console.log(response);
     })
+
+    $("#submit").on("click", function(event) {
+        // Don't refresh the page!
+        event.preventDefault();
+    
+
+            //var clnc = event.target.options[event.target.selectedIndex].dataset.clnc;
+            //console.log("rc: " + rc);
+            //console.log("clnc: " + clnc);
+
+        //console.log(this.options[this.selectedIndex]);
+        // Code in the logic for storing and retrieving the most recent user.
+        var tempMin = $('#temp option:selected').attr('data-min');
+        var tempMax = $('#temp option:selected').attr('data-max');
+
+        var massMin = $('#mass option:selected').attr('data-min');
+        var massMax = $('#mass option:selected').attr('data-max');
+
+        var orbitMin = $('#orbit option:selected').attr('data-min');
+        var orbitMax = $('#orbit option:selected').attr('data-max');
+        console.log(tempMin, tempMax, massMin, massMax,orbitMin, orbitMax);
+  
+  });
+//"http://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=planets&constraint=st_teffstr%20between%200%20and%205075"
