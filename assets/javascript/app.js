54e0e85bbf37e5b14c7ae664a1d3d46969eafdc9
Spencer Caretta <spencer.caretta@gmail.com>
2018-11-05 19:18:08 -0600

    some css and our API appending to table in js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+
 var queryURL = "";
 var config = {
     apiKey: "EBb20IKo5DUliI4QmZnwQQ4sfvUKIcqfgHdePwb3",
@@ -23,26 +24,27 @@ $("#submit").on("click", function(event) {
     var orbitMin = $('#orbit option:selected').attr('data-min');
     var orbitMax = $('#orbit option:selected').attr('data-max');
     console.log(tempMin, tempMax, massMin, massMax,orbitMin, orbitMax);
-    queryURL = "http://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=planets&constraint=st_teffstr%20between%20" + tempMin + "%20and%20" + tempMax + "&constraint=pl_bmassjstr%20between%20" + massMin + "%20and%20" + massMax + "&constraint=pl_orbperstr%20between%20" + orbitMin + "%20and%20" + orbitMax;
+    queryURL = "https://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=planets&constraint=st_teffstr%20between%20" + tempMin + "%20and%20" + tempMax + "&constraint=pl_bmassjstr%20between%20" + massMin + "%20and%20" + massMax + "&constraint=pl_orbperstr%20between%20" + orbitMin + "%20and%20" + orbitMax;
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    
+    })
+        .then(function(response){
+            //var results = response.data;
+            var foo = JSON.parse(response)
+            console.log(foo);
+            boo = foo[0].pl_name;
+            console.log(boo);
+    
+            for (i = 0; i < foo.length; i++) {
+            $("#planets").append("<tr><td>" + foo[i].pl_name + "</td><td>" + foo[i].pl_orbper + "</td><td>" + foo[i].pl_bmassj + "</td><td>" + foo[i].st_dist + "</td><td>" + foo[i].st_teff + "</tr>");  
+            }
+        });
 });
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
 
-})
-    .then(function(response){
-        //var results = response.data;
-        var foo = JSON.parse(response)
-        console.log(foo);
-        boo = foo[0].pl_name;
-        console.log(boo);
-
-        for (i = 0; i < foo.length; i++) {
-        $("#planets").append("<tr><td>" + foo[i].pl_name + "</td><td>" + foo[i].pl_orbper + "</td><td>" + foo[i].pl_bmassj + "</td><td>" + foo[i].st_dist + "</td><td>" + foo[i].st_teff + "</tr>");  
-        }
-    });
 
 
 //"http://exoplanetarchive.ipac.caltech.edu/cgi-bin/TblView/nph-tblView?app=ExoTbls&config=planets&constraint=st_teffstr%20between%200%20and%205075"
