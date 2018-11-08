267f52651f7cd405e22c175972966af4de47912f
Patrick Keane <pkeane92@yahoo.com>
2018-11-07 23:41:21 -0600

    fixed the reset and the get more planet info button seems to work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -161,22 +161,22 @@ database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", functi
         }
     });        
 
+    var url = "";
+    console.log(planetName);
+    
+    $("#moreinfo").on("click", function(){
+           console.log("hello");
+           url =  "https://exoplanets.nasa.gov/search.html?q=" + planetName;
+           window.open (url, '_blank');
+    });
+
 });
 
-
-
-var url = "";
-console.log(planetName);
-
-$("#moreinfo").on("click", function(){
-       console.log("hello");
-       url =  "https://exoplanets.nasa.gov/search.html?q=" + planetname;
-       window.open (url, '_blank');
-});
 
 $("#reset").on("click", function(event3) {
    event3.preventDefault();
    $("#planetinfo").empty();
+   $("#planetinfo").append("<div class='row'> <div class='col-md-12'><table class='table table-bordered' id='planets'> </table> </div> </div>")
 })
 
 
